a1df913a9cea399ede81e383e0f81fedb40ba5e2
Yudinda Gilang Pramudya <pramudyagilang23@gmail.com>
2025-12-21 18:20:46 +0700

    Update __init__.py

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,6 +1,6 @@
-from .advisor_agent import AdvisorAgent
 from .rag_agent import RAGAgent
 from .sql_agent import SQLAgent
+from .advisor_agent import AdvisorAgent
 from .cover_letter_agent import CoverLetterAgent
 from .interview_agent import InterviewAgent
 from .orchestrator import Orchestrator
